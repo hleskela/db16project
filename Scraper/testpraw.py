0c59b2683be4372d139d336465d487e5dad5f079
Hannes Leskelä <hleskela@kth.se>
2016-05-09 14:30:27 +0200

    Edit so that the script prints comments from threads
    It seems like the iterator gets messed up if you print both the thread
    name and the comments with list comprehensions. It is probably due to
    my noobish ways, but for now let's keep what works. Will format the
    output so it comes in the style that we want.

--- Scraper/testpraw.py
+++ Scraper/testpraw.py
@@ -1,22 +1,33 @@
 import praw
+
+def print_thread(obj):
+    print(str(obj))
+
+def print_comments(post):
+    print("in print_comments")
+    for comment in post:
+        print("in print_comments for loop")
+        print(comment.body)
+
 print("Initializing crawler...")
 user_agent = "DB16app by DB16scraper, woo us!"
-r = praw.Reddit(user_agent=user_agent)
 subreddits = ["todayilearned", "science", "askscience","space","AskHistorians","YouShouldKnow","explainlikeimfive"]
+r = praw.Reddit(user_agent=user_agent)
 print("Fetching subreddit posts...")
 top_posts_per_subreddit = [ r.get_subreddit(x).get_hot(limit=5) for x in subreddits ]
 
 for subreddit in top_posts_per_subreddit:
     print("Post:")
-    posts = [str(post) for post in subreddit]
-    print(posts)
-    for submission in subreddit:
-        print("Comments:")
-        thread = next(submission)
-        print(thread.comments)
+    #threads = [print_thread(thread) for thread in subreddit]
+    for thread in subreddit:
+        i = 0
+        print(i)
+        print(thread)
+        for comment in thread.comments:
+            if not isinstance(comment, praw.objects.MoreComments):
+                print(comment.body)
+            print(i)
+            i = i+1
+    print("Comment:")
+   
 
-    submissions = r.get_subreddit('python').get_top(limit=10)
-    hannes = next(submissions)
-    flat_comments = praw.helpers.flatten_tree(hannes.comments)
-    for comment in flat_comments:
-        print(comment)
