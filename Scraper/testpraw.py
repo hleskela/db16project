0480d52bc8fee10f69b0bee742cafd0705f9f3f0
Sarah Berenji <sarahba@kth.se>
2016-05-10 13:34:42 +0200

    Add the subreddit name column

--- Scraper/testpraw.py
+++ Scraper/testpraw.py
@@ -15,7 +15,7 @@ subreddits = ["AskReddit","funny","todayilearned","pics","science","worldnews","
 r = praw.Reddit(user_agent=user_agent)
 top_posts_per_subreddit = [ r.get_subreddit(x).get_top_from_year(limit=100) for x in subreddits ]
 
-POST_ID = 0 
+POST_ID = 0
 
 for subreddit in top_posts_per_subreddit:
     #threads = [print_thread(thread) for thread in subreddit]
@@ -23,11 +23,11 @@ for subreddit in top_posts_per_subreddit:
         i = 0
         real_comments = [comment for comment in thread.comments if isinstance(comment, praw.objects.Comment)]
         real_comments.sort(key=lambda comment: comment.score, reverse=True)
-        
+
         for comment in real_comments:
             user = comment.author
-            print(str(POST_ID) + ",\"" + str(thread.title).replace("\"","\"\"") + "\"," + str(user) + ",\""+ str(comment.body).replace("\"","\"\"")+ "\"")
+            print(str(POST_ID) + "," + str(comment.subreddit.display_name) + ",\"" + str(thread.title).replace("\"","\"\"") + "\"," + str(user) + ",\""+ str(comment.body).replace("\"","\"\"")+ "\"")
             POST_ID = POST_ID+1
             i = i+1
-   
+
 
