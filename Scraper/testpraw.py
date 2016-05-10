72f4d6a62fbb2504b3c63d96ed9d329b2bfacb3a
Hannes Leskelä <hleskela@kth.se>
2016-05-10 12:03:17 +0200

    Expands the #subreddits scraped, remove printouts

--- Scraper/testpraw.py
+++ Scraper/testpraw.py
@@ -1,4 +1,5 @@
 import praw
+import re
 
 def print_thread(obj):
     print(str(obj))
@@ -9,25 +10,24 @@ def print_comments(post):
         print("in print_comments for loop")
         print(comment.body)
 
-print("Initializing crawler...")
 user_agent = "DB16app by DB16scraper, woo us!"
-subreddits = ["todayilearned", "science", "askscience","space","AskHistorians","YouShouldKnow","explainlikeimfive"]
+subreddits = ["AskReddit","funny","todayilearned","pics","science","worldnews","IAmA","announcements","videos","gaming","blog","movies","Music","aww","news","gifs","explainlikeimfive","askscience","EarthPorn","books","television","LifeProTips","mildlyinteresting","DIY","sports","Showerthoughts","space","Fitness","tifu","Jokes","InternetIsBeautiful","food","photoshopbattles","gadgets","history","nottheonion","dataisbeautiful","Futurology","GetMotivated","Documentaries","personalfinance","listentothis","philosophy","UpliftingNews","OldSchoolCool","The_Donald","TrollXChromosomes","TwoXChromosomes","SandersForPresident", "BlackPeopleTwitter"]
 r = praw.Reddit(user_agent=user_agent)
-print("Fetching subreddit posts...")
-top_posts_per_subreddit = [ r.get_subreddit(x).get_hot(limit=5) for x in subreddits ]
+top_posts_per_subreddit = [ r.get_subreddit(x).get_top_from_year(limit=100) for x in subreddits ]
+
+POST_ID = 0 
 
 for subreddit in top_posts_per_subreddit:
-    print("Post:")
     #threads = [print_thread(thread) for thread in subreddit]
     for thread in subreddit:
         i = 0
-        print(i)
-        print(thread)
-        for comment in thread.comments:
-            if not isinstance(comment, praw.objects.MoreComments):
-                print(comment.body)
-            print(i)
+        real_comments = [comment for comment in thread.comments if isinstance(comment, praw.objects.Comment)]
+        real_comments.sort(key=lambda comment: comment.score, reverse=True)
+        
+        for comment in real_comments:
+            user = comment.author
+            print(str(POST_ID) + ",\"" + str(thread.title).replace("\"","\"\"") + "\"," + str(user) + ",\""+ str(comment.body).replace("\"","\"\"")+ "\"")
+            POST_ID = POST_ID+1
             i = i+1
-    print("Comment:")
    
 
